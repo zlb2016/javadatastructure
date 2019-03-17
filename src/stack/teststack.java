5dc08b5ac6def607b93ea1ed231831b8ec3a450f
zlb <l>
2019-03-17 11:10:05 +0800

    提交stack的测试方法

--- src/stack/teststack.java
+++ src/stack/teststack.java
@@ -3,10 +3,12 @@ public class teststack {
     public static void main(String[] args) {
         // write your code here
         ArrayStack as=new ArrayStack(3);
-        as.push(1);
+        as.push(1);//入栈
         as.push(2);
         as.push(3);
-        as.display();
+        as.peek();//取栈顶元素
+        as.pop();//出栈
+        as.display();//展示栈内元素
         //System.out.println(as.display());
     }
 }
